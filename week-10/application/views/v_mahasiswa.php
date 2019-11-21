0dc3359ffaca4a601c5c52817419c3865fa017f5
unknown <anaz.nazrull.m@gmail.com>
2019-11-21 21:27:09 +0700

    Add File

---
+++ week-10/application/views/v_mahasiswa.php
@@ -0,0 +1,15 @@
+
+<html>
+	<head>
+	</head>
+	<body>
+		<tbody>
+			<?php
+				echo 'NIM	 : <strong>'.$nim.'</strong><br>';
+				echo 'Nama	 : <strong>'.$nama.'</strong><br>';
+				echo 'Alamat : <strong>'.$alamat.'</strong><br>';
+				echo 'TTL	 : <strong>'.$TTL.'</strong><br>';
+			?>
+		</tbody>
+	</body>
+</html>
